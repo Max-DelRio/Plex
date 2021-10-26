8263588e77c151e009a696553d78b1faefcc2a0e
KristinannOsborn <60935679+KristinannOsborn@users.noreply.github.com>
2021-10-26 11:51:55 -0700

    integrated login, sign out, and user roles

---
+++ Plex/public/js/admin.js
@@ -0,0 +1,45 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuEr3fsBnIi0TtGkQKNghven49vzcV6CY",
+  authDomain: "plex-inventory-sytem.firebaseapp.com",
+  projectId: "plex-inventory-sytem",
+  storageBucket: "plex-inventory-sytem.appspot.com",
+  messagingSenderId: "858484179975",
+  appId: "1:858484179975:web:df507d461da15a4baf56a2"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            const uid = user.uid;
+            console.log(uid);
+            var docRef = db.collection("Users").doc(uid);
+            docRef.get().then((doc) => {
+                if(doc.exists){
+                    console.log(doc);
+                    var role = doc.data().role;
+                    if(role == "Employee")
+                    {
+                        document.getElementById("um").style.display = "none";
+                    }
+                    else if(role == "Admin")
+                    {
+                        document.getElementById("um").style.display = "block";
+                    }
+                }
+            });
+        } else {
+          // User not logged in or has just logged out.
+        }
+      });
+
+});
