87b28cd82e8aa43dc0dc4c47d6b4eadeff4c8317
Max Del Rio <maxdelrio7@gmail.com>
2021-11-13 20:51:09 -0800

    Added Create Order button

--- Plex/public/js/tracking.js
+++ Plex/public/js/tracking.js
@@ -50,8 +50,8 @@ function queryDB(){
                 td[0].textContent = count;
                 td[1].textContent = doc.data().orderno;
                 td[2].textContent = doc.data().product;
-                td[3].textContent = doc.data().station;
-                td[4].textContent = doc.data().status;
+                td[3].textContent = doc.data().status;
+                td[4].textContent = doc.data().instation;
                 td[5].textContent = id;
     
                 tbody.appendChild(clone);
@@ -73,89 +73,89 @@ function queryDB(){
     });
 }
 
-document.querySelector("#addProduct").addEventListener("submit", function AddProduct(e) {
+document.querySelector("#addOrder").addEventListener("submit", function AddOrder(e) {
         e.preventDefault();
-        const name = document.getElementById("name").value;
-        const ID = document.getElementById("ID").value;
-        const category = document.getElementById("category").value;
-        const quantity = document.getElementById("quantity").value;
+        const orderno = document.getElementById("orderno").value;
+        const product = document.getElementById("product").value;
+        const status = document.getElementById("status").value;
+        const instation = document.getElementById("instation").value;
         
         wait();
         async function wait(){
-            let result = await Promise.resolve(db.collection("Products").add({name: name, id: ID, category: category, quantity: quantity}));
+            let result = await Promise.resolve(db.collection("Tracking").add({orderno: orderno, product: product, status: status, instation: instation}));
             location.reload();
         }
 });
 
-document.querySelector("#editProduct").addEventListener("submit", function EditProduct(e) {
-    e.preventDefault();
-    console.log("edit"+edit);
-    const name = document.getElementById("ename").value;
-    const ID = document.getElementById("eID").value;
-    const category = document.getElementById("ecategory").value;
-    const quantity = document.getElementById("equantity").value;
-    wait();
-    async function wait(){    
-        const res = await Promise.resolve(db.collection("Products").doc(edit).update({name: name, id: ID, category: category, quantity: quantity}));
-        location.reload();
-    }       
+// document.querySelector("#editProduct").addEventListener("submit", function EditProduct(e) {
+//     e.preventDefault();
+//     console.log("edit"+edit);
+//     const name = document.getElementById("ename").value;
+//     const ID = document.getElementById("eID").value;
+//     const category = document.getElementById("ecategory").value;
+//     const quantity = document.getElementById("equantity").value;
+//     wait();
+//     async function wait(){    
+//         const res = await Promise.resolve(db.collection("Products").doc(edit).update({name: name, id: ID, category: category, quantity: quantity}));
+//         location.reload();
+//     }       
     
-});
+// });
 
-document.querySelector("#clear").onclick = function(){
-    $(row).css("background-color", "#fdfdfe");
-    console.log(row);
-    edit = "";
-};
+// document.querySelector("#clear").onclick = function(){
+//     $(row).css("background-color", "#fdfdfe");
+//     console.log(row);
+//     edit = "";
+// };
 
-document.querySelector("#filter").addEventListener("click", function table(e){
-    e.preventDefault();
-    var select = document.getElementById("sortCate");
-    var category = select.value;
-    var equals = "==";
-    var query = document.getElementById("query").value;
-    var ref = db.collection("Products");
-    var result = ref.where(category,equals,query);
-    count = 1;
-    document.getElementById("table").innerHTML="";
-    result.get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            const id = doc.id;
-            console.log(doc.id, " => ", doc.data().name, doc.data().id, doc.data().category, doc.data().quantity);
-            if('content' in document.createElement('template')) {
-                var tbody = document.querySelector("#table");
-                var template = document.querySelector("#productRow");
-                var clone = template.content.cloneNode(true);
-                var tr = clone.querySelector("tr");
-                var td = clone.querySelectorAll("td");
-                td[0].textContent = count;
-                td[1].textContent = doc.data().name;
-                td[2].textContent = doc.data().id;
-                td[3].textContent = id;
+// document.querySelector("#filter").addEventListener("click", function table(e){
+//     e.preventDefault();
+//     var select = document.getElementById("sortCate");
+//     var category = select.value;
+//     var equals = "==";
+//     var query = document.getElementById("query").value;
+//     var ref = db.collection("Products");
+//     var result = ref.where(category,equals,query);
+//     count = 1;
+//     document.getElementById("table").innerHTML="";
+//     result.get().then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             const id = doc.id;
+//             console.log(doc.id, " => ", doc.data().name, doc.data().id, doc.data().category, doc.data().quantity);
+//             if('content' in document.createElement('template')) {
+//                 var tbody = document.querySelector("#table");
+//                 var template = document.querySelector("#productRow");
+//                 var clone = template.content.cloneNode(true);
+//                 var tr = clone.querySelector("tr");
+//                 var td = clone.querySelectorAll("td");
+//                 td[0].textContent = count;
+//                 td[1].textContent = doc.data().name;
+//                 td[2].textContent = doc.data().id;
+//                 td[3].textContent = id;
     
-                tbody.appendChild(clone);
-            }
-            count++;
-        });
-        var table = document.getElementById("products");
-        var rows = table.querySelectorAll("tr");
-        for(var i = 1; i < rows.length; i++){
-            rows[i].onclick = function(){
-                row = this;
-                $(this).css("background-color", "#eaeaeb");
-                document.getElementById("ename").value = this.cells[1].innerHTML;
-                document.getElementById("eID").value = this.cells[2].innerHTML;
-                edit = this.cells[3].innerHTML;
-            }
-        }
-    });
-});
+//                 tbody.appendChild(clone);
+//             }
+//             count++;
+//         });
+//         var table = document.getElementById("products");
+//         var rows = table.querySelectorAll("tr");
+//         for(var i = 1; i < rows.length; i++){
+//             rows[i].onclick = function(){
+//                 row = this;
+//                 $(this).css("background-color", "#eaeaeb");
+//                 document.getElementById("ename").value = this.cells[1].innerHTML;
+//                 document.getElementById("eID").value = this.cells[2].innerHTML;
+//                 edit = this.cells[3].innerHTML;
+//             }
+//         }
+//     });
+// });
 
-document.querySelector("#clearFilter").addEventListener("click", function table(e){
-    e.preventDefault();
-    document.getElementById("table").innerHTML="";
-    queryDB();
-});
+// document.querySelector("#clearFilter").addEventListener("click", function table(e){
+//     e.preventDefault();
+//     document.getElementById("table").innerHTML="";
+//     queryDB();
+// });
 
 
 
